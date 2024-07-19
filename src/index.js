03df9a9871143f29a08702f31fde439e4988fcc7
SABELOMAWELA <Mawelas63@gmail.com>
2024-07-19 05:26:52 +0200

    "Updated index.html and src/index.js with changes to product list and modal functionality"

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ const storeData = () => ({
     products: [],
   },
   loading: true,
+  showModal: false,
+  modalProduct: null,
+  openModal(item) {
+    this.modalProduct = item;
+    this.showModal = true;
+  },
   init() {
     this.loading = true;
     fetch("https://fakestoreapi.com/products")
@@ -20,3 +26,9 @@ const storeData = () => ({
 document.addEventListener('alpine:init', () => {
   Alpine.data('storeData', storeData);
 });
+
+
+  function Refresh() {
+    showModal = false;
+    window.location.reload();
+  }
